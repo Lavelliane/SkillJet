5b79a0b1e7846ef3c9618ceee403a8671db2dce0
Lavelliane <jhurylastre432@gmail.com>
2023-05-09 07:56:51 +0800

    other utils and alpine

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class ListingController extends Controller
 {
     //
     public function index(){
         return view('listings.index', [
-            'listings' => Listing::latest()->filter(request(['tag', 'search']))->get()
+            'listings' => Listing::latest()->filter(request(['tag', 'search']))->paginate(4)
         ]);
     }
     public function show(Listing $listing){
@@ -23,6 +24,17 @@ class ListingController extends Controller
     }
 
     public function store(Request $request){
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => ['required', Rule::unique('listings','company')],
+            'location' => 'required',
+            'website' => 'required',
+            'email' => ['required', 'email'],
+            'tags' => 'required',
+            'description' => 'required'
+        ]);
+        Listing::create($formFields);
         
+        return redirect('/')->with('message', 'Listing Created Successfully');
     }
 }
